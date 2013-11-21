8963dc95885bfab7bd89e3d253536489b1cbaa23
Andrew Kitchell <akitchell@gmail.com>
2013-11-21 11:17:22 -0800

    Changes port that ilab listens on:: 6000

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ var app = module.exports = express();
  */
 
 // all environments
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 6000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(express.logger('dev'));
